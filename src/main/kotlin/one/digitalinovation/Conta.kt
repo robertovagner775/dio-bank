a4aad27f3bc73d87bbc1b3c79fcf2152bc7494e9
Roberto <robertovagner04@gmail.com>
2022-06-11 21:55:03 -0300

    create a conta class

---
+++ src/main/kotlin/one/digitalinovation/Conta.kt
@@ -0,0 +1,17 @@
+package one.digitalinovation
+
+import java.math.BigDecimal
+
+class Conta {
+
+    val agencia: String = ""
+    val numero: String = ""
+    val saldo : BigDecimal = TODO()
+
+}
+fun deposito(valor: BigDecimal) {
+
+}
+fun saque(valor: BigDecimal){
+
+}
--- src/main/kotlin/one/digitalinovation/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/Pessoa.kt
@@ -2,14 +2,21 @@ package one.digitalinovation
 
 class Pessoa {
 
-    var nome:String = "Roberto"
-    var cpf:String = "111.111.111-11"
+      var nome:String = "Roberto"
+      var cpf:String = "111.111.111-11"
+      private set
 
+      constructor()
 
+      fun uniNomeCpf() = "$nome  $cpf"
 }
 fun main(){
 
     var roberto = Pessoa()
+
+
+    println(roberto.uniNomeCpf())
     println(roberto.nome)
     println(roberto.cpf)
+
 }
