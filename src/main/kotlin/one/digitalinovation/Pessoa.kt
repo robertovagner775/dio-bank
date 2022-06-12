32fbf16752cdad812f5b939c5f158d13c4fcd359
Roberto <robertovagner04@gmail.com>
2022-06-11 21:00:41 -0300

    classe Pessoa

---
+++ src/main/kotlin/one/digitalinovation/Pessoa.kt
@@ -0,0 +1,15 @@
+package one.digitalinovation
+
+class Pessoa {
+
+    var nome:String = "Roberto"
+    var cpf:String = "111.111.111-11"
+
+
+}
+fun main(){
+
+    var roberto = Pessoa()
+    println(roberto.nome)
+    println(roberto.cpf)
+}
