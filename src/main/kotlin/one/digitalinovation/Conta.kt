30a2d76e184529f0fce2b01f0bd522bfbd77e9eb
Roberto <robertovagner04@gmail.com>
2022-06-13 20:19:27 -0300

    create a conta data class

--- src/main/kotlin/one/digitalinovation/Conta.kt
+++ src/main/kotlin/one/digitalinovation/Conta.kt
@@ -2,16 +2,18 @@ package one.digitalinovation
 
 import java.math.BigDecimal
 
-class Conta {
+class Conta (
 
-    val agencia: String = ""
-    val numero: String = ""
-    val saldo : BigDecimal = TODO()
+    val agencia: String,
+    val numero: String,
+    val saldo : BigDecimal
 
-}
-fun deposito(valor: BigDecimal) {
+    ) {
+    fun deposito(valor: BigDecimal) {
 
-}
-fun saque(valor: BigDecimal){
+    }
 
+    fun saque(valor: BigDecimal) {
+
+    }
 }
