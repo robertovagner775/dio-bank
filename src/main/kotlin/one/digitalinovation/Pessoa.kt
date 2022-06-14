79fe5eb66da7daa7abf72ff76862c79c6f24b0b7
Roberto <robertovagner04@gmail.com>
2022-06-13 23:08:24 -0300

    testes feitos com a classe funcionario e criação da propria

--- src/main/kotlin/one/digitalinovation/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/Pessoa.kt
@@ -1,22 +1,6 @@
 package one.digitalinovation
 
-class Pessoa {
-
-      var nome:String = "Roberto"
-      var cpf:String = "111.111.111-11"
-      private set
-
-      constructor()
-
-      fun uniNomeCpf() = "$nome  $cpf"
-}
-fun main(){
-
-    var roberto = Pessoa()
-
-
-    println(roberto.uniNomeCpf())
-    println(roberto.nome)
-    println(roberto.cpf)
-
-}
+open class Pessoa(
+    open val nome:String,
+    open val cpf:String
+)
